72442d99d9befd15ef28232095eedd7b41b463f6
Matt Gross <mbgross111@gmail.com>
2021-01-14 22:15:09 -0800

    add date picker and audit tasks by due date

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,10 @@ var createTask = function(taskText, taskDate, taskList) {
   var taskP = $("<p>")
     .addClass("m-1")
     .text(taskText);
-
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
-
-
+  // check due date
+  auditTask(taskLi);
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -33,7 +32,6 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -133,12 +131,19 @@ $(".list-group").on("click", "span", function() {
     .val(date);
   // swap out elements
   $(this).replaceWith(dateInput);
+  //enable jQuery ui datepicker
+  dateInput.datepicker({
+    minDate: 0,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
   // auto focus
   dateInput.trigger("focus");
 });
 
 // due date change
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -161,6 +166,8 @@ $(".list-group").on("blur", "input[type='text']", function() {
     .text(date);
   // replace input with the spanEl
   $(this).replaceWith(taskSpan);
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 //~~~~~EDIT TASK END~~~~~
 
@@ -168,24 +175,16 @@ $(".list-group").on("blur", "input[type='text']", function() {
 // sortable widget https://api.jqueryui.com/sortable/
 $(".card .list-group").sortable({
   connectWith: $(".card .list-group"),
-  scroll: true,
+  scroll: false,
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    // console.log("~~~~~ activate ~~~~~")
-    // console.log(this);
   },
   deactivate: function(event) {
-    // console.log("~~~~~ deactivate ~~~~~")
-    // console.log(this);
   },
   over: function(event) {
-    // console.log("~~~~~ over ~~~~~")
-    // console.log(event.target);
   },
   out: function(event) {
-    // console.log("~~~~~ out ~~~~~")
-    // console.log(event.target);
   },
   update: function(event) {
     // array to store the task data in
@@ -224,14 +223,33 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function(event, ui) {
-    console.log("over");
   },
   out: function(event, ui) {
-    console.log("out");
   }
 });
-
 //~~~~~DRAG AND DROP END~~~~~
+
+//date picker widget https://api.jqueryui.com/datepicker/
+$("#modalDueDate").datepicker({
+  minDate: 0
+});
+
+//audit tasks with moment.js
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  // remove any old classes from the element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+  // apply new class if task is near/over due date
+  if(moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } 
+  else if(Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
